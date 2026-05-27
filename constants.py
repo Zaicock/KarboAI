f411df133a6d1dd5e2ae51376c0d688bc09c80e1
dev ghazawi vip <81128163+Zaicock@users.noreply.github.com>
2026-05-27 07:22:52 +0300

    Add constants for API base URL and default headers

---
+++ constants.py
@@ -0,0 +1,12 @@
+BASE_URL = "https://api.karboai.com"
+
+DEFAULT_HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (Linux; Android 10; K) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/139.0.0.0 Mobile Safari/537.36"
+    ),
+    "origin": "https://karboai.com",
+    "referer": "https://karboai.com/",
+    "accept-language": "ar-US,ar;q=0.9,en-US;q=0.8,en;q=0.7"
+}
