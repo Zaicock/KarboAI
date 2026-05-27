9a6d991f5108ff62c4384d02ea3d0ad04e8c3968
dev ghazawi vip <81128163+Zaicock@users.noreply.github.com>
2026-05-27 07:24:45 +0300

    Implement KarboAI class with HTTP GET method

---
+++ client.py
@@ -0,0 +1,36 @@
+import requests
+
+from .constants import BASE_URL, DEFAULT_HEADERS
+from .exceptions import RequestError
+
+from .community import CommunityAPI
+from .blogs import BlogsAPI
+
+
+class KarboAI:
+    def __init__(self, timeout=30):
+        self.base_url = BASE_URL
+        self.timeout = timeout
+
+        self.session = requests.Session()
+        self.session.headers.update(DEFAULT_HEADERS)
+
+        self.community = CommunityAPI(self)
+        self.blogs = BlogsAPI(self)
+
+    def get(self, endpoint, params=None):
+        url = f"{self.base_url}{endpoint}"
+
+        try:
+            response = self.session.get(
+                url,
+                params=params,
+                timeout=self.timeout
+            )
+
+            response.raise_for_status()
+
+            return response.json()
+
+        except requests.RequestException as e:
+            raise RequestError(str(e))
